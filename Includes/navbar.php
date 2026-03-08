6ff6955d60a448faeb17253593ba3b3f49f69899
Your Name <you@example.com>
2026-03-08 06:44:12 +0300

    MuhammadJavohir03

---
+++ Includes/navbar.php
@@ -0,0 +1,48 @@
+<?php
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
+
+<div class="container">
+    <header class="sticky-top d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+
+        <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <span class="fs-4 text-black">
+                <h1><img class="" style="height:60px; with:60px;" src="Logos/Logo.jpg" alt="">-Registrator</h1>
+            </span>
+        </a>
+
+
+
+        <ul class="nav nav-pills">
+
+            <li class="nav-item">
+                <a href="index.php"
+                    class="nav-link shadow-sm <?= ($current_page == 'index.php') ? 'active' : '' ?>">
+                    Bosh Sahifa
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="tekshiruv.php"
+                    class="nav-link shadow-sm <?= ($current_page == 'tekshiruv.php') ? 'active' : '' ?>">
+                    Tekshiruv
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="academic.php"
+                    class="nav-link shadow-sm <?= ($current_page == 'academic.php') ? 'active' : '' ?>">
+                    Academic Policy
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="about.php"
+                    class="nav-link shadow-sm <?= ($current_page == 'about.php') ? 'active' : '' ?>">
+                    Biz Haqimizda
+                </a>
+            </li>
+
+        </ul>
+    </header>
+</div>
---
+++ about.php
@@ -0,0 +1,125 @@
+<?php $title = "Bosh Sahifa"; ?>
+<?php require "Includes/header.php"; ?>
+
+<body>
+    <?php require "Includes/navbar.php"; ?>
+
+    <div class="container">
+        <div class="bg-body-tertiary m-5 p-3 rounded-2 rounded-1 text-center">
+            <h1 class="text-danger">Registrator Ofisi Vazifasi</h1>
+            <h5>Talabalarning o'quv jarayonida vujudga kelgan savol, yordam va ko'rsatmalar bo'yicha ko'mak beruvchi bo'lim</h5>
+            <h3>Qabul kunlari:</h3>
+            <h5>Dushanba - Juma (8:30-17:30)</h5>
+            <h3>Aloqa:</h3>
+            <h5>+(998) 73 545-33-33</h5>
+        </div>
+
+        <div class="row text-center m-3">
+            <div class="col">
+                <img class="shadow rounded-circle profile-img1" src="Reg_office/Ikramov_MuhammadYusuf.jpg" alt="">
+                <h1 class="fw-normal">Ikramov MuhammadYusuf Abdulaxad o'g'li</h1>
+                <h3>Registrator ofisi boshlig'i</h3>
+                <div class="accordion" id="accordionExample">
+                    <div class="text-center">
+                        <button class="btn bg-danger text-white" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Batafsil
+                        </button>
+                    </div>
+                    <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <strong>
+                                Muhammad Yusuf Ikramov shu kunga qadar Qo‘qon universitetining Iqtisodiyot va ta'lim fakulteti ish yurituvchisi, fakultet dekani muovini mas'ul vazifalarida ishlagan.
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <h1 class="text-center m-5">Registrator Hero bo'limi</h1>
+            <div class="row text-center ">
+                <div class="col">
+                    <img class="shadow rounded-circle profile-img" src="Reg_office/Mulaydinov Farxod Murotovich.jpg" alt="">
+                    <h3 class="fw-normal">Mulaydinov Farxod Murotovich</h3>
+                    <h5>Registrator ofisi Hero Mutaxasisi</h5>
+                </div><!-- /.col-lg-4 -->
+
+                <div class="col">
+                    <img class="shadow rounded-circle profile-img" src="Reg_office/Diyorbek_Hero.jpg" alt="">
+                    <h3 class="fw-normal">Xo’jamurodov Diyorbek Ulug’bek o’g’li</h3>
+                    <h5>Registrator ofisi Hero Mutaxasisi</h5>
+                </div><!-- /.col-lg-4 -->
+
+                <div class="col">
+                    <img class="shadow rounded-circle profile-img" src="Reg_office/Toxirjonov_Donyor.jpg" alt="">
+                    <h3 class="fw-normal">Toxirjonov Doniyor Baxtiyor o‘g‘li</h3>
+                    <h5>Registrator ofisi Hero Mutaxasisi</h5>
+                </div><!-- /.col-lg-4 -->
+            </div>
+
+            <div class="row text-center m-5">
+                <div class="col">
+                    <img class="shadow rounded-circle profile-img" src="Reg_office/Mulaydinov Farxod Murotovich.jpg" alt="">
+                    <h3 class="fw-normal">Baxtiyorjonov MuhammadJavoxir Jamshid o'g'li</h3>
+                    <h5>Registrator ofisi Hero Mutaxasisi</h5>
+                </div><!-- /.col-lg-4 -->
+
+                <div class="col">
+                    <img class="shadow rounded-circle profile-img" src="Reg_office/Diyorbek_Hero.jpg" alt="">
+                    <h3 class="fw-normal">Saydaliyev Abdulazizxon</h3>
+                    <h5>Registrator ofisi Hero Mutaxasisi</h5>
+                </div><!-- /.col-lg-4 -->
+
+                <div class="col">
+                    <img class="shadow rounded-circle profile-img" src="Reg_office/Toxirjonov_Donyor.jpg" alt="">
+                    <h3 class="fw-normal">Begzodbek</h3>
+                    <h5>Registrator ofisi Hero Mutaxasisi</h5>
+                </div><!-- /.col-lg-4 -->
+            </div>
+
+            <h1 class="text-center m-5">Registrator Asosiy shtabi</h1>
+            <div class="row text-center m-5">
+                <div class="col">
+                    <img class="shadow rounded-circle profile-img" src="Reg_office/Munavvar.jpg" alt="">
+                    <h3 class="fw-normal">Xatamqulov Munavvarjon Maxsutali o‘g‘li</h3>
+                    <h5>Registrator ofisi Asosiy shtabi</h5>
+                </div><!-- /.col-lg-4 -->
+
+                <div class="col">
+                    <img class="shadow rounded-circle profile-img" src="Reg_office/Diyor.jpg" alt="">
+                    <h3 class="fw-normal">Zaylobiddinov Diyorbek Elyorjon o’g’li</h3>
+                    <h5>Registrator ofisi Asosiy shtabi</h5>
+                </div><!-- /.col-lg-4 -->
+
+                <div class="col">
+                    <img class="shadow rounded-circle profile-img" src="Reg_office/laziz.jpg" alt="">
+                    <h3 class="fw-normal">Oxunov Lazizxon Avazxon o`g`li</h3>
+                    <h5>Registrator ofisi Asosiy shtabi</h5>
+                </div><!-- /.col-lg-4 -->
+            </div>
+
+            <div class="row text-center">
+                <div class="col">
+                    <img class="shadow rounded-circle profile-img" src="Reg_office/yaxyo.jpg" alt="">
+                    <h3 class="fw-normal">Sodiqjonov Yaxyobek Sobirjon o`gli</h3>
+                    <h5>Registrator ofisi Asosiy shtabi</h5>
+                </div><!-- /.col-lg-4 -->
+
+                <div class="col">
+                    <img class="shadow rounded-circle profile-img" src="Reg_office/ali.jpg" alt="">
+                    <h3 class="fw-normal">Aliyev Alibek Sobir o`g`li</h3>
+                    <h5>Registrator ofisi Asosiy shtabi</h5>
+                </div><!-- /.col-lg-4 -->
+
+                <div class="col">
+                    <img class="shadow rounded-circle profile-img" src="Reg_office/mohim.jpg" alt="">
+                    <h3 class="fw-normal">Xasanova Mohimbegim Raxmonjon qizi</h3>
+                    <h5>Registrator ofisi Asosiy shtabi</h5>
+                </div><!-- /.col-lg-4 -->
+            </div>
+
+        </div>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+<?php require "Includes/footer.php"; ?>
