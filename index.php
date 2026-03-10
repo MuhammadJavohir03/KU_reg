dd14bba102bf0e353e1a47aec9dc32b836d006bd
MuhammadJavohir03 <javohir8386@gmail.com>
2026-03-10 14:21:29 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php $title = "Bosh Sahifa"; ?>
+<?php require "Includes/header.php"; ?>
+
+<body>
+    <?php require "Includes/navbar.php"; ?>
+    <div class="container">
+        <div id="carouselExampleCaptions" class="carousel slide bg-dark" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            </div>
+            <div class="carousel-inner my-4">
+
+                <div class="carousel-item active">
+                    <div class="position-relative bg-dark opacity-50">
+                        <a href="elon1.php">
+                            <img src="Logos/1.jpg" class="d-block w-100" style="height:400px; object-fit:cover;" alt="...">
+                        </a>
+
+                    </div>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Oliy taʼlim, fan va innovatsiyalar vazirligining navbatdagi Hayʼat yigʻilishi oʻtkazildi</h5>
+                        <p>2026-yil 2-mart kunining ikkinchi yarmida Qoʻqon shahrida Oliy taʼlim, fan va innovatsiyalar vazirligining navbatdagi Hayʼat yigʻilishi boʻlib oʻtdi. Unda respublikaning davlat, nodavlat va xorijiy oliy taʼlim tashkilotlari rahbarlari, shuningdek, vazirlik markaziy apparati, tizim tashkilotlari rahbarlari ishtirok etdi. Hayʼat yigʻilishiga vazir Qoʻngʻirotboy Sharipov raislik qildi. Yigʻilishda 2026-yilga moʻljallangan ustuvor vazifalar, davlat dasturlari ijrosi, kasbiy va oliy taʼlim tizimidagi islohotlar, ilmiy-innovatsion faoliyat hamda bitiruvchilar bandligini taʼminlash masalalari muhokama qilindi.</p>
+                    </div>
+                </div>
+
+                <div class="carousel-item active">
+                    <div class="position-relative bg-dark opacity-50">
+                        <a href="elon2.php">
+                            <img src="Logos/2.jpg" class="d-block w-100" style="height:400px; object-fit:cover;" alt="...">
+                        </a>
+                    </div>
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Qoʻqon universitetida amalga oshirilayotgan ijtimoiy hamkorlik loyihalari, ehtiyojmand oilalar farzandlarini oʻqitish tajribasi va uni joriy etish mexanizmlariga bagʻishlangan seminar oʻtkazildi</h5>
+                        <p>2-mart kuni Qoʻqon shahridagi Qoʻqon universitetida yoshlar siyosati, zamonaviy taʼlim va ijtimoiy qoʻllab-quvvatlash mexanizmlarini kengaytirishga bagʻishlangan seminar boʻlib oʻtdi.
+                            Yigʻilishda 24-fevral kuni Prezident Shavkat Mirziyoyev raisligida oʻtkazilgan yoshlarning tashabbuslarini qoʻllab-quvvatlash boʻyicha qoʻshimcha imkoniyatlarga bagʻishlangan yigʻilishda belgilangan vazifalar ijrosi muhokama qilindi.</p>
+                    </div>
+                </div>
+
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+        
+        
+        <?php require "yonalishlar.php"; ?>
+       
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+<?php require "Includes/footer.php"; ?>
