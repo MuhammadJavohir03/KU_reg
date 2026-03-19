6752fc027056bdc75f251276411f0892469d07b7
MuhammadJavohir03 <javohir8386@gmail.com>
2026-03-19 17:10:32 +0300

    update files

--- elon1.php
+++ elon1.php
@@ -3,17 +3,32 @@
 <body>
     <?php require "Includes/navbar.php"; ?>
 
-    <div class="container">
+    <style>
+        .bg-image {
+            background-image: url('Logos/bepul.jpg');
+            background-size: cover;
+            background-position: center;
+            height: 70vh;
+            display: block;
+        }
+    </style>
+
+    <div class="container blur45 p-4 bg-image">
         <h1>Qayta topshirish (bahorgi bepul imkoniyat)</h1>
         <h3>Bahorgi maktabga qabul qilinish uchun joriy va oraliq ortacha foizi 33% dan yuqori bo'lishi, ushbu fandan qoldirilgan darslar % ham 32% dan oshmagan bo'lishi va bir yilda 3 martadan ushbu arizaga topshirmagan bo'lishi kerak.
-        </h3><br><h1>Ariza topshirish tartibi</h1>
-        <br><h2>1.</h2><h3>Ushbu sahifa yuqori qismidagi "Arizalar" bo'limiga o'ting.</h3>
-        <br><h2>2.</h2><h3>Qayta topshirish(Bepul Imkoniyat) bo'limini tanlang.</h3>
-        <br><h2>3.</h2><h3>Ma'lumotlarni kiriting. Agar siz bergan ma'lumotlar xato bo'lsa yoki oxirgacha to'ldirilmasa sizning arizangiz qabul qilinmaydi.</h3>
+        </h3><br>
+        <h1>Ariza topshirish tartibi</h1>
+        <br>
+        <h2>1.</h2>
+        <h3>Ushbu sahifa yuqori qismidagi "Arizalar" bo'limiga o'ting.</h3>
+        <br>
+        <h2>2.</h2>
+        <h3>Qayta topshirish(Bepul Imkoniyat) bo'limini tanlang.</h3>
+        <br>
+        <h2>3.</h2>
+        <h3>Ma'lumotlarni kiriting. Agar siz bergan ma'lumotlar xato bo'lsa yoki oxirgacha to'ldirilmasa sizning arizangiz qabul qilinmaydi.</h3>
         <div class="row">
-            <img class="rounded-5 my-4  d-block" src="Logos/bepul.jpg" alt="">
-
-            <p>KU Xabarlar 2-mart</p>
+            <!-- <img class="rounded-5 my-4  d-block" src="Logos/bepul.jpg" alt=""> -->
         </div>
 
 
