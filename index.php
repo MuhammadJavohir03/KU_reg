4ee5d907aa7b82d939a0a061fbada93696f68c05
MuhammadJavohir03 <javohir8386@gmail.com>
2026-03-19 16:53:06 +0300

    update files

--- index.php
+++ index.php
@@ -14,17 +14,17 @@
                 <div class="carousel-item active">
                     <div class="position-relative bg-dark opacity-50">
                         <a href="elon1.php">
-                            <img src="Logos/1.jpg" class="d-block w-100" style="height:400px; object-fit:cover;" alt="...">
+                            <img src="Logos/bepul.jpg" class="d-block w-100" style="height:400px; object-fit:cover;" alt="...">
                         </a>
 
                     </div>
                     <div class="carousel-caption d-none d-md-block">
-                        <h5>Oliy taʼlim, fan va innovatsiyalar vazirligining navbatdagi Hayʼat yigʻilishi oʻtkazildi</h5>
-                        <p>2026-yil 2-mart kunining ikkinchi yarmida Qoʻqon shahrida Oliy taʼlim, fan va innovatsiyalar vazirligining navbatdagi Hayʼat yigʻilishi boʻlib oʻtdi. Unda respublikaning davlat, nodavlat va xorijiy oliy taʼlim tashkilotlari rahbarlari, shuningdek, vazirlik markaziy apparati, tizim tashkilotlari rahbarlari ishtirok etdi. Hayʼat yigʻilishiga vazir Qoʻngʻirotboy Sharipov raislik qildi. Yigʻilishda 2026-yilga moʻljallangan ustuvor vazifalar, davlat dasturlari ijrosi, kasbiy va oliy taʼlim tizimidagi islohotlar, ilmiy-innovatsion faoliyat hamda bitiruvchilar bandligini taʼminlash masalalari muhokama qilindi.</p>
+                        <h5>Qayta topshiruv bahorgi (bepul imkoniyat) maktabiga ariza berish boshlandi</h5>
+                        <p>Qayta topshirishga ariza berish 21-mart 2026-yilgacha amal qiladi shoshiling! </p>
                     </div>
                 </div>
 
-                <div class="carousel-item active">
+                <!-- <div class="carousel-item active">
                     <div class="position-relative bg-dark opacity-50">
                         <a href="elon2.php">
                             <img src="Logos/2.jpg" class="d-block w-100" style="height:400px; object-fit:cover;" alt="...">
@@ -35,7 +35,7 @@
                         <p>2-mart kuni Qoʻqon shahridagi Qoʻqon universitetida yoshlar siyosati, zamonaviy taʼlim va ijtimoiy qoʻllab-quvvatlash mexanizmlarini kengaytirishga bagʻishlangan seminar boʻlib oʻtdi.
                             Yigʻilishda 24-fevral kuni Prezident Shavkat Mirziyoyev raisligida oʻtkazilgan yoshlarning tashabbuslarini qoʻllab-quvvatlash boʻyicha qoʻshimcha imkoniyatlarga bagʻishlangan yigʻilishda belgilangan vazifalar ijrosi muhokama qilindi.</p>
                     </div>
-                </div>
+                </div> -->
 
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -47,7 +47,7 @@
                 </button>
             </div>
         </div>
-        <?php require "yonalishlar.php"; ?>
+        <!-- <?php require "yonalishlar.php"; ?> -->
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
